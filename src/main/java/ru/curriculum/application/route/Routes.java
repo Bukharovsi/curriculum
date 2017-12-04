b821e18aa4ee30db93494e8bf729e3b2a8756d56
Kuznetsov Vladislav <vladislav.kuznetsov@rtlabs.ru>
2017-12-04 22:18:25 +0300

    CURRICULUM-1. В личный кабинет добавлен раздел упралвения пользователями

--- src/main/java/ru/curriculum/application/route/Routes.java
+++ src/main/java/ru/curriculum/application/route/Routes.java
@@ -6,4 +6,5 @@ public class Routes {
     public static final String admin = "/admin";
     public static final String loginError = "/login-error";
     public static final String accessDenied = "/access-denied";
+    public static final String users = "/admin/users";
 }
