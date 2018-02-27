52c26d4f6ec995876418edc9f2be33d6c9687fda
kuznetsov.vladislav <vladislav.kuznetsov@rtlabs.ru>
2018-02-27 23:17:21 +0300

    simple levenshtein

--- src/main/java/ru/curriculum/lib/DahmerauLevenshtein.java
+++ src/main/java/ru/curriculum/lib/DahmerauLevenshtein.java
@@ -7,32 +7,63 @@ public class DahmerauLevenshtein {
 
         for(int i = 0; i <= s1.length(); i++) {
             for(int j = 0; j <= s2.length(); j++) {
-                if(i == 0 && j == 0) {
-                    matrix[i][j] = 0;
+//                if(i == 0 && j == 0) {
+//                    matrix[i][j] = 0;
+//                    continue;
+//                }
+//
+//                if(i == 0 && j > 0) {
+//                    matrix[i][j] = j;
+//                    continue;
+//                }
+//
+//                if(i > 0 && j == 0) {
+//                    matrix[i][j] = i;
+//                    continue;
+//                }
+                if(firstCase(i, j)) {
+                    matrix[i][j] = minForFirstCase(i, j);
                     continue;
                 }
 
-                if(i == 0 && j > 0) {
-                    matrix[i][j] = j;
+                if(secondCase(i, j, s1, s2)) {
+                    matrix[i][j] = minForSecondCase(i, j, matrix, s1, s2);
                     continue;
                 }
 
-                if(i > 0 && j == 0) {
-                    matrix[i][j] = i;
-                    continue;
-                }
-
-                matrix[i][j] = min(i, j, matrix, s1, s2);
+                matrix[i][j] = minForThirdCase(i, j, matrix, s1, s2);
             }
         }
 
         return matrix[s1.length()][s2.length()];
     }
 
-    private int min(int i, int j, int[][] matrix, String s1, String s2) {
+    private boolean firstCase(int i, int j) {
+        return 0 == Math.min(i, j);
+    }
+
+    private int minForFirstCase(int i, int j) {
+        return Math.max(i, j);
+    }
+
+    private boolean secondCase(int i, int j, String s1, String s2) {
+        return i > 1 && j > 1
+                && s1.charAt(i - 1) == s2.charAt(j - 2)
+                && s2.charAt(j - 1) == s1.charAt(i - 2);
+    }
+
+    private int minForSecondCase(int i, int j, int[][] matrix, String s1, String s2) {
+        int minByLivenshtein = minForThirdCase(i, j, matrix, s1, s2);
+        int additionalDahmerau = matrix[i-2][j-2] + 1;
+
+        return Math.min(minByLivenshtein, additionalDahmerau);
+    }
+
+    private int minForThirdCase(int i, int j, int[][] matrix, String s1, String s2) {
         int first = matrix[i][j - 1] + 1;
         int second = matrix[i - 1][j] + 1;
         int third = matrix[i - 1][j - 1] + operationWeight(s1.charAt(i - 1), s2.charAt(j - 1));
+
         return Math.min(Math.min(first, second), third);
     }
 
--- src/test/java/ru/curriculum/domain/lib/DahmerauLevenshteinTest.java
+++ src/test/java/ru/curriculum/domain/lib/DahmerauLevenshteinTest.java
@@ -16,13 +16,29 @@ public class DahmerauLevenshteinTest {
     public void findTest() {
 //        String s1 = "POLYNOMIAL";
 //        String s2 = "EXPONENTIAL";
-        String s1 = "очное";
-        String s2 = "очная";
+        String s1 = "ape";
+        String s2 = "ea";
 
         int [][] a = new int[s1.length() + 1][s2.length() + 1];
 
         int result = this.dahmerauLevenshtein.find(s1, s2);
 
         System.out.println(result);
+    }
+
+    @Test
+    public void test() {
+        compare("baba", "arab");
+        compare("contest", "toner");
+        compare("martial", "marital");
+        compare("monarchy", "democracy");
+        compare("seatback", "backseat");
+        compare("warfare", "farewell");
+        compare("smoking", "hospital");
+        compare("ape", "ea");
+    }
+
+    private void compare(String s1 ,String s2) {
+        System.out.println(s1 + " " + s2 + " " + dahmerauLevenshtein.find(s1, s2));
     }
 }
